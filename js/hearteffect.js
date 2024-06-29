c6b94ec0d0be392da9f605933c9140e6161391b2
sacheerc <madushansachintha@gmail.com>
2024-06-29 20:57:43 +0530

    Menu modification with scrolling effect

--- js/hearteffect.js
+++ js/hearteffect.js
@@ -197,3 +197,18 @@ function createDiv(height, width) {
   div.style.backgroundColor = 'transparent';
   return div;
 }
+
+$(document).ready(function () {
+  $('a[href^="#"]').on('click', function (event) {
+    var target = $(this.getAttribute('href'));
+    if (target.length) {
+      event.preventDefault();
+      $('html, body').stop().animate(
+        {
+          scrollTop: target.offset().top,
+        },
+        1000
+      );
+    }
+  });
+});
